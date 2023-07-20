ccd5371f3120d3869e6878f35295c69b1443d389
mullermu <44443947+mullermu@users.noreply.github.com>
2023-07-20 21:15:57 +0700

    Update swg.py 20230720

--- swg.py
+++ swg.py
@@ -21,7 +21,6 @@ class Swg(object):
 
         #concat data and dataset for Scaler
         df = pd.concat([df, SWG_Data],axis=0, ignore_index=True)
-        st.table(df.head())
 
         scl = StandardScaler()
         

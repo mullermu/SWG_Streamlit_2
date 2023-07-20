2d950d47aabf97330da5cfd6e3b52ddcff071922
mullermu <44443947+mullermu@users.noreply.github.com>
2023-07-20 20:48:59 +0700

    Update model_predict.py 20230720

--- model_predict.py
+++ model_predict.py
@@ -6,7 +6,6 @@ import joblib
 import streamlit as st
 import swg
 class get_predict_result(object):
-    
     def getResult(clfmodel, data, sim = False):
         def get_file(path_dataset):
             files = os.listdir(path_dataset)
@@ -32,15 +31,12 @@ class get_predict_result(object):
                 filename = file.split("/")[-1].split(".")[0]
                 if filename != 'A16':
                     rawdf=pd.read_csv(file, index_col=0)
-
+                        
                     rs = swg.Swg(rawdf)
                     X = rs.scale()
-
                     model = joblib.load(os.path.join("model/",clfmodel))
                     z = model.predict(X)
-
-                    
-                    res = pd.concat([rawdf,pd.DataFrame(z+1,columns=['Status'])],axis=1)
+                    res = pd.concat([rawdf,pd.DataFrame(z,columns=['Status'])],axis=1)
                     res.insert(0, "Panel", filename, allow_duplicates=True)
                     
                     if export :     
@@ -51,16 +47,21 @@ class get_predict_result(object):
                     df = pd.read_csv(file)#, usecols=read_columns)
 
         def predict_sim():
+            
             rawdf=data
+
             rs = swg.Swg(rawdf)
-            X = rs.scale()
-            predict_df = pd.DataFrame(X).iloc[:1]
+            predict_df = rs.scale()
+            
+            # predict_df = pd.DataFrame(X).iloc[:1]
+            
             model = joblib.load(os.path.join("model/",clfmodel))
             z = model.predict(predict_df)
             res = pd.concat([predict_df,pd.DataFrame(z+1,columns=['Status'])],axis=1)
             res.insert(0, "Panel", "Simulation", allow_duplicates=True)
-
+            
             st.write(res['Status'])
+
         if sim is not True:
             predict_and_save(export = True)
         else:

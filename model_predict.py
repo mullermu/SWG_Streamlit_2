7750239d333deb0c7d862d9722953bd1012e2b41
mullermu <44443947+mullermu@users.noreply.github.com>
2024-10-18 09:57:30 +0700

    Update model_predict.py

--- model_predict.py
+++ model_predict.py
@@ -24,7 +24,11 @@ class get_predict_result(object):
             return clf
         def predict_and_save(export = False):
             path_sheet  = "Temp/"
-            read_columns = ["A6_Upper_Bus_PhaseA","A6_Upper_Bus_PhaseB","A6_Upper_Bus_PhaseC",
+            
+            read_columns = ["Start","UPPER_BUS_PHASE_C","OUTGOING_PHASE_B","UPPER_BUS_PD"
+            ,"LOWER_BUS_PD","SPOUT_PD","OUTGOING_PD"]
+            
+            # read_columns = ["A6_Upper_Bus_PhaseA","A6_Upper_Bus_PhaseB","A6_Upper_Bus_PhaseC",
                 "A6_Lower_Bus_PhaseA","A6_Lower_Bus_PhaseB","A6_Lower_Bus_PhaseC",
                 "A6_Outgoing_PhaseA","A6_Outgoing_PhaseB","A6_Outgoing_PhaseC",
                 "PD_Max_Upper", "PD_Max_Lower", "PD_Max_Spout", "PD_Max_Outgoing"]
@@ -35,16 +39,17 @@ class get_predict_result(object):
                 filename = file.split("/")[-1].split(".")[0]
                 if filename != 'A16':
                     rawdf=pd.read_csv(file, index_col=0)
-                    rawdf=rawdf.rename(columns={'UPPER_BUS_PHASE_A':'A6_Upper_Bus_PhaseA','UPPER_BUS_PHASE_B':'A6_Upper_Bus_PhaseB', 
+                    # rawdf=rawdf.rename(columns={'UPPER_BUS_PHASE_A':'A6_Upper_Bus_PhaseA','UPPER_BUS_PHASE_B':'A6_Upper_Bus_PhaseB', 
                          'UPPER_BUS_PHASE_C':'A6_Upper_Bus_PhaseC','LOWER_BUS_PHASE_A':'A6_Lower_Bus_PhaseA',
                          'LOWER_BUS_PHASE_B':'A6_Lower_Bus_PhaseB','LOWER_BUS_PHASE_C':'A6_Lower_Bus_PhaseC',
                          'OUTGOING_PHASE_A':'A6_Outgoing_PhaseA','OUTGOING_PHASE_B':'A6_Outgoing_PhaseB',
                          'OUTGOING_PHASE_C':'A6_Outgoing_PhaseC','UPPER_BUS_PD':'PD_Max_Upper','LOWER_BUS_PD':'PD_Max_Lower',
                          'SPOUT_PD':'PD_Max_Spout','OUTGOING_PD':'PD_Max_Outgoing'})
-                    # rs = swg.Swg(rawdf)
-                    # X = rs.scale()
+                    rs = swg.Swg(rawdf)
+                    X = rs.scale()
                     model = joblib.load(os.path.join("model/",clfmodel))
-                    z = model.predict(rawdf[read_columns])
+                    # z = model.predict(rawdf[read_columns])
+                    z = model.predict(X)
                     res = pd.concat([rawdf,pd.DataFrame(z+1,columns=['Status'])],axis=1)
                     res.insert(0, "Panel", filename, allow_duplicates=True)
                     
@@ -76,7 +81,6 @@ class get_predict_result(object):
             
             model = joblib.load(os.path.join("model/",clfmodel))
             z = model.predict(rawdf[read_columns])
-            st.write(z)
             res = pd.concat([rawdf,pd.DataFrame(z+1,columns=['Status'])],axis=1)
             res.insert(0, "Panel", "Simulation", allow_duplicates=True)
             
